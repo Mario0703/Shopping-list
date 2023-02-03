f7b9831420aae2e5bf390de72ece15c6452b248f
Mario <mario@supermario.dk>
2023-02-03 16:47:33 +0100

    Added style, and fixed button positions

--- index.js
+++ index.js
@@ -11,23 +11,24 @@ BTN.onclick = function(){
 function CreateElements(){
     
     let li = document.createElement("li");
-    let p = document.createElement("p")
+    let span = document.createElement("span")
     const edit_BTN = document.createElement("button");
     const del_BTN = document.createElement("button");
 
-    del_BTN.innerText = "delete";
-    edit_BTN.innerText = "EDIT";
+
+    del_BTN.innerText = "Delete";
+    edit_BTN.innerText = "Edit";
 
     del_BTN.className = "delete"
-
-    p.innerText = input.value;
-    li.appendChild(p);
+    edit_BTN.className = "edit"
+    span.innerText = input.value;
+    li.appendChild(span);
     li.appendChild(edit_BTN);
     li.appendChild(del_BTN);
     ul.appendChild(li);
-    input.value = ""
-    
+    li.style.listStyleType = "none";
 
+    input.value = ""
     ul.addEventListener("click", deleteAfterClick);
 
     function deleteAfterClick(event) {
@@ -36,22 +37,20 @@ function CreateElements(){
             event.target.closest("li").remove();
         }
     }
-
-    
-    
       
     EDIT();
     
     function EDIT(){
         edit_BTN.onclick = function(){
-            if(p.contentEditable == "true"){
-                edit_BTN.innerText = "EDIT"
-                p.contentEditable = "false";
+            if(span.contentEditable == "true"){
+                span.contentEditable = "false";
+                edit_BTN.innerText = "Edit"
             }
             else{
-                edit_BTN.innerText = "confirm"
-                p.contentEditable = "true";
+                span.contentEditable = "true";
+                edit_BTN.innerText = "Confirm"
             }
+            console.log(span.contentEditable)
         }
     }
 
